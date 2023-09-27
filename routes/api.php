beb5f3807e234b4675b0d2ccccb34ce39ceb2c6e
Adil omer <36453919+adelazhary@users.noreply.github.com>
2023-09-27 20:27:29 +0800

    updating the api route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\V1\Admin\TravelController as AdminTravelController;
 use App\Http\Controllers\Api\V1\TourController;
 use App\Http\Controllers\Api\V1\TravelController;
 use Illuminate\Http\Request;
@@ -23,5 +24,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('travels', [TravelController::class, 'index']);
 Route::get('travels/{travel:slug}/tours', [TourController::class, 'index']);
 
+Route::prefix('admin')->group(function () {
+    Route::post('travels', [AdminTravelController::class, 'store']);
+});
 
 
